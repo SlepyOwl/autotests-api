05d442b2eefdb62c9947c98b1d14632541f5d573
SlepyOwl <terebonkivich@mail.ru>
2026-09-21 18:15:09 +0300

    Введение в Pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,10 @@
+
+def test_first_try():
+    print("Щкорпион теиру")
+
+def test_assert_positive_case():
+    assert (2+2) == 4
+    
+def test_assert_negative_case():
+    x, y = 5, 2
+    assert (y + y) == x, "Anosa"
